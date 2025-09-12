2de2366b3c90fa8253b0acff59a75653ddc61bca
Scott Stephens <stephens.js@gmail.com>
2025-09-11 21:10:21 -0500

    nixos/tpm2: use redirection instead of cat
    Co-authored-by: Philip Taron <philip.taron@gmail.com>

--- nixos/modules/security/tpm2.nix
+++ nixos/modules/security/tpm2.nix
@@ -302,7 +302,7 @@ in
 
                 # if file exists, read old hash
                 if [ -f $hashFile ]; then
-                  oldHash=$(cat $hashFile)
+                  oldHash="$(< $hashFile)"
                 else
                   oldHash=""
                 fi
